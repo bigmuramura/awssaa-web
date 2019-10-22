8a17c253f0bb32c552699845165fbb7eba696ce2
Yasutaka Ohmura <nishikimachi.hokkaido@gmail.com>
2019-10-22 21:04:26 +0900

    テストでVPC作成

--- README.md
+++
@@ -1,1 +0,0 @@
-# awssaa-web
---
+++ main.tf
@@ -0,0 +1,17 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+  
+provider "aws" {
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "awssaa-web-vpc" {
+    cidr_block = "172.16.0.0./16"
+    tags = {
+        Name = "awssaa-web-vpc"
+    }
+}
+
+
