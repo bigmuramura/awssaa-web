c4b836b49f923f37e920550eb9a1370ae526ffd1
Yasutaka Ohmura <nishikimachi.hokkaido@gmail.com>
2019-10-25 23:22:16 +0900

    スペルミス修正

--- main.tf
+++ main.tf
@@ -77,6 +77,6 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private-subnet.id
-  route_table_id = aws_route_table.private-rt
+  route_table_id = aws_route_table.private-rt.id
 }
 
