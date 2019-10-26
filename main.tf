4926a397c8b119c4d75bea0dd187f66c06198214
Yasutaka Ohmura <nishikimachi.hokkaido@gmail.com>
2019-10-26 11:24:20 +0900

    スペルミス修正

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "aws_eip" "nat-gateway-eip" {
 }
 
 resource "aws_nat_gateway" "nat-gateway" {
-  allocation_id = aws_eip.naigateway-eip.id
+  allocation_id = aws_eip.nat-gateway-eip.id
   subnet_id     = aws_subnet.private-subnet.id
   depends_on    = [aws_internet_gateway.igw]
 }
