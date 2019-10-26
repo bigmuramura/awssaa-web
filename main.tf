cf66519d89039035c66ed65afc428ba135692433
Yasutaka Ohmura <nishikimachi.hokkaido@gmail.com>
2019-10-26 16:17:39 +0900

    タグ削除

--- main.tf
+++ main.tf
@@ -99,7 +99,4 @@ resource "aws_nat_gateway" "nat-gateway" {
   allocation_id = aws_eip.nat-gateway-eip.id
   subnet_id     = aws_subnet.private-subnet.id
   depends_on    = [aws_internet_gateway.igw]
-  tags = {
-    Name = "awssaa-nat-gateway"
-  }
 }
