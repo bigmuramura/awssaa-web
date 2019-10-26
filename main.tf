67291736a04224ca432946e24327eb0b1091c347
Yasutaka Ohmura <nishikimachi.hokkaido@gmail.com>
2019-10-26 11:22:29 +0900

    スペルミス修正

--- main.tf
+++ main.tf
@@ -69,6 +69,12 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private-rt.id
+  gateway_id             = aws_nat_gateway.nat-gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 # サブネットとルートテーブルの紐付け
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public-subnet.id
@@ -91,6 +97,6 @@ resource "aws_eip" "nat-gateway-eip" {
 
 resource "aws_nat_gateway" "nat-gateway" {
   allocation_id = aws_eip.naigateway-eip.id
-  subnet_id     = aws_subnet.private-subnet.subnet_id
+  subnet_id     = aws_subnet.private-subnet.id
   depends_on    = [aws_internet_gateway.igw]
 }
