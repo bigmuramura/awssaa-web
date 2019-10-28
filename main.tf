98e546918647bc6260e5f3f634334000feafc27f
Yasutaka Ohmura <nishikimachi.hokkaido@gmail.com>
2019-10-28 23:01:34 +0900

    マルチAZ!

--- main.tf
+++ main.tf
@@ -18,18 +18,28 @@ resource "aws_vpc" "awssaa-vpc" {
 }
 
 # サブネット作成 Public
-resource "aws_subnet" "public-subnet" {
+resource "aws_subnet" "public-subnet-1a" {
   vpc_id                  = aws_vpc.awssaa-vpc.id
   cidr_block              = "172.16.10.0/24"
   map_public_ip_on_launch = true # インスタンスにパブリックIP自動割り当て有効
   availability_zone       = "ap-northeast-1a"
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1c" {
+  vpc_id                  = aws_vpc.awssaa-vpc.id
+  cidr_block              = "172.16.11.0/24"
+  map_public_ip_on_launch = true # インスタンスにパブリックIP自動割り当て有効
+  availability_zone       = "ap-northeast-1c"
+  tags = {
+    Name = "public-subnet-1c"
   }
 }
 
 # サブネット作成 Private
-resource "aws_subnet" "private-subnet" {
+resource "aws_subnet" "private-subnet-1a" {
   vpc_id                  = aws_vpc.awssaa-vpc.id
   cidr_block              = "172.16.20.0/24"
   map_public_ip_on_launch = false # インスタンスにパブリックIP自動割り当てない
@@ -39,6 +49,15 @@ resource "aws_subnet" "private-subnet" {
   }
 }
 
+resource "aws_subnet" "private-subnet-1c" {
+  vpc_id                  = aws_vpc.awssaa-vpc.id
+  cidr_block              = "172.16.21.0/24"
+  map_public_ip_on_launch = false # インスタンスにパブリックIP自動割り当てない
+  availability_zone       = "ap-northeast-1c"
+  tags = {
+    Name = "private-subnet"
+  }
+}
 # インターネットゲートウェイ作成
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.awssaa-vpc.id
@@ -55,14 +74,6 @@ resource "aws_route_table" "public-rt" {
   }
 }
 
-# ルートテーブル作成 Private
-resource "aws_route_table" "private-rt" {
-  vpc_id = aws_vpc.awssaa-vpc.id
-  tags = {
-    Name = "awssaa-private-rt"
-  }
-}
-
 # ルーティング設定 Public
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public-rt.id
@@ -70,38 +81,80 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-# サブネットとルートテーブルの紐付け
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public-subnet.id
+# サブネットとルートテーブルの紐付け Public
+resource "aws_route_table_association" "public-1a" {
+  subnet_id      = aws_subnet.public-subnet-1a.id
   route_table_id = aws_route_table.public-rt.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private-subnet.id
-  route_table_id = aws_route_table.private-rt.id
+resource "aws_route_table_association" "public-1c" {
+  subnet_id      = aws_subnet.public-subnet-1c.id
+  route_table_id = aws_route_table.public-rt.id
 }
 
 # NATゲートウェイ作成 Privateサブネット用
-## 先にNATゲートウェイ用のEIPを作成する必要がある
-resource "aws_eip" "nat-gateway-eip" {
+# 先にNATゲートウェイ用のEIPを作成する必要がある
+resource "aws_eip" "nat-gateway-eip1" {
   vpc        = true
   depends_on = [aws_internet_gateway.igw]
-  # memo NATゲートウェイは暗黙的にIGWに依存している。
-  #      depends onでIGW作成後にNATゲートウェイ作成することを保証する
+  # NATゲートウェイは暗黙的にIGWに依存している。
+  # depends onでIGW作成後にNATゲートウェイ作成することを保証する
 }
 
-resource "aws_nat_gateway" "nat-gateway" {
-  allocation_id = aws_eip.nat-gateway-eip.id
-  subnet_id     = aws_subnet.public-subnet.id # NATゲートウェイ自体はPublicに置かれる
+resource "aws_eip" "nat-gateway-eip2" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# NATゲートウェイ作成
+resource "aws_nat_gateway" "nat-gateway-1a" {
+  allocation_id = aws_eip.nat-gateway-eip1.id
+  subnet_id     = aws_subnet.public-subnet-1a.id # NATゲートウェイ自体はPublicに置かれる
   depends_on    = [aws_internet_gateway.igw]
   tags = {
-    Name = "awssaa-nat-gateway"
+    Name = "awssaa-nat-gateway-1a"
   }
 }
+resource "aws_nat_gateway" "nat-gateway-1c" {
+  allocation_id = aws_eip.nat-gateway-eip2.id
+  subnet_id     = aws_subnet.public-subnet-1c.id # NATゲートウェイ自体はPublicに置かれる
+  depends_on    = [aws_internet_gateway.igw]
+  tags = {
+    Name = "awssaa-nat-gateway-1c"
+  }
+}
+# ルートテーブル作成 Private
+resource "aws_route_table" "private-rt1" {
+  vpc_id = aws_vpc.awssaa-vpc.id
+  tags = {
+    Name = "awssaa-private-rt1"
+  }
 
+}resource "aws_route_table" "private-rt2" {
+  vpc_id = aws_vpc.awssaa-vpc.id
+  tags = {
+    Name = "awssaa-private-rt2"
+  }
+}
 # ルーティング設定 Private
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private-rt.id
-  nat_gateway_id         = aws_nat_gateway.nat-gateway.id
+resource "aws_route" "private-1a" {
+  route_table_id         = aws_route_table.private-rt1.id
+  nat_gateway_id         = aws_nat_gateway.nat-gateway-1a.id
   destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private-1c" {
+  route_table_id         = aws_route_table.private-rt1.id
+  nat_gateway_id         = aws_nat_gateway.nat-gateway-1c.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private-subnet-1a.id
+  route_table_id = aws_route_table.private-rt1.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private-subnet-1c.id
+  route_table_id = aws_route_table.private-rt2.id
 }
